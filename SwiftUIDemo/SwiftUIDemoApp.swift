6b9df5dc369dd41bedfc60bef10cc7212a749f87
Theik Chan <theikchan.tc@gmail.com>
2023-04-16 07:47:38 +0630

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Theik Chan on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
